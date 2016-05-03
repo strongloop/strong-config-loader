4975c3fb64b8441256e8685cd1fbf0cd0b57aebb
Ryan Graham <r.m.graham@gmail.com>
2016-05-03 15:43:57 -0700

    update copyright notices

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+// Copyright IBM Corp. 2013. All Rights Reserved.
+// Node module: strong-config-loader
+// This file is licensed under the Artistic License 2.0.
+// License text available at https://opensource.org/licenses/Artistic-2.0
+
 /**
  * config-loader ~ public api
  */
